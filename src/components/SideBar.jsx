c20bd96100db5c2c2972784cb2988afec8f535a7
Namgyal Tamang <75193697+Namgyal86@users.noreply.github.com>
2024-10-28 22:15:29 +0545

    uploading

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar(){
+    return(
+        <div>
+            <h2>The Brutal Martian landscpate</h2>
+            <div>
+                <p>Description</p>
+                <p>dasf asfddskhfka asdfhkajsdf</p>
+            </div>
+        </div>
+    )
+}
